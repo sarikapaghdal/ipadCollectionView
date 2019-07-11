07823f2c32b03ff3ae45d4ed5d3874aa75d150e2
sarika <sarika.paghdal1@gmail.com>
2019-07-11 12:22:16 +0200

    Initial Commit

---
+++ ipadCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ipadCollection
+//
+//  Created by Dhaval_G_S on 11.07.19.
+//  Copyright © 2019 Dhaval_G_S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ipadCollectionTests/ipadCollectionTests.swift
@@ -0,0 +1,34 @@
+//
+//  ipadCollectionTests.swift
+//  ipadCollectionTests
+//
+//  Created by Dhaval_G_S on 11.07.19.
+//  Copyright © 2019 Dhaval_G_S. All rights reserved.
+//
+
+import XCTest
+@testable import ipadCollection
+
+class ipadCollectionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
